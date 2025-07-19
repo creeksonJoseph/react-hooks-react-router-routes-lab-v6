f125abc955b6c60f13eb5e029f18c63fb2d506a9
creeksonjoseph <charanajoseph@gmail.com>
2025-07-19 09:47:57 +0300

    e

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 import { useEffect, useState } from "react";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then(setMovies);
+  }, []);
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
+      <NavBar />
+      <h1>Home Page</h1>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} id={movie.id} title={movie.title} />
+      ))}
     </>
   );
-};
+}
 
 export default Home;
